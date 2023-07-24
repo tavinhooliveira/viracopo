33c4b2e96320683d7560d3cb22024486b028e10e
Luis Otavio N Oliveira <tavinhomegas18@gmail.com>
2023-07-24 20:45:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let numeroPremiado; // Variável para armazenar o número premiado
+let numeroEscolhido; // Variável para armazenar o número escolhido pelo jogador
 
 // Função para sortear um número aleatório entre 0 e 50
 function sortearNumero() {
   const numeroSorteado = Math.floor(Math.random() * 51);
-  console.log('Número sorteado:', numeroSorteado);
+  console.log('Numero sorteado:', numeroSorteado);
   return numeroSorteado;
 }
 
@@ -11,35 +12,12 @@ function sortearNumero() {
 function resetarJogo() {
   numeroPremiado = sortearNumero();
   document.getElementById('result').innerText = '';
+  numeroEscolhido = null; // Limpar o número escolhido pelo jogador
   const numberButtons = document.getElementsByClassName('numberBtn');
   for (let i = 0; i < numberButtons.length; i++) {
     const button = numberButtons[i];
     button.disabled = false;
     button.style.backgroundColor = '#ffcc00';
-  }
-}
-
-// Função para verificar o número escolhido
-function verificarEscolha(button) {
-  const numeroEscolhido = parseInt(button.innerText);
-
-  // Verificar se o número escolhido é o número premiado
-  if (numeroEscolhido === numeroPremiado) {
-    document.getElementById('result').innerText = 'Acertou Mizeravi...  ' + numeroPremiado;
-    button.style.backgroundColor = '#00cc00'; // Tornar o botão verde
-    document.getElementById('numberButtons').style.display = 'none';
-    document.getElementById('choppImg').classList.remove('hidden');
-
-    // Desabilitar os demais botões
-    const numberButtons = document.getElementsByClassName('numberBtn');
-    for (let i = 0; i < numberButtons.length; i++) {
-      const button = numberButtons[i];
-      button.disabled = true;
-    }
-  } else {
-    console.log('errou!' + numeroEscolhido);
-    button.style.backgroundColor = '#ff0000'; // Tornar o botão vermelho
-    button.disabled = true; // Desabilitar o botão errado
   }
 }
 
@@ -66,6 +44,61 @@ function generateNumberButtons() {
     button.classList.add('numberBtn');
     button.innerText = i.toString().padStart(2, '0'); // Adiciona um zero à esquerda se o número for menor que 10
     numberButtonsContainer.appendChild(button);
+  }
+}
+
+function verificarEscolha(button) {
+  numeroEscolhido = parseInt(button.innerText);
+
+  const numberButtons = document.getElementsByClassName('numberBtn');
+
+  if (numeroEscolhido < numeroPremiado) {
+    // Desativar os números inferiores ao número escolhido ou até o 00
+    if (numeroEscolhido < numeroPremiado) {
+      for (let i = 0; i <= numeroEscolhido ; i++) {
+        const button = numberButtons[i];
+        button.style.backgroundColor = '#b5b4b4'; // Tornar o botão vermelho
+        button.disabled = true;
+      }
+    } else {
+      for (let i = 0; i <= 50; i++) {
+        const button = numberButtons[i];
+        button.style.backgroundColor = '#b5b4b4'; // Tornar o botão vermelho
+        button.disabled = true;
+      }
+    }
+  }
+
+
+  if (numeroEscolhido > numeroPremiado) {
+      // Desativar os números superiores ao número escolhido ou até o 50
+    if (numeroEscolhido > numeroPremiado) {
+      for (let i = numeroEscolhido; i <= 50; i++) {
+        const button = numberButtons[i];
+        button.style.backgroundColor = '#b5b4b4'; // Tornar o botão vermelho
+        button.disabled = true;
+      }
+    } else {
+      for (let i = numeroPremiado + 1; i <= 50; i++) {
+        const button = numberButtons[i];
+        button.style.backgroundColor = '#b5b4b4'; // Tornar o botão vermelho
+        button.disabled = true;
+      }
+    }
+  }
+
+  // Verificar se o número escolhido é o número premiado
+  if (numeroEscolhido === numeroPremiado) {
+    document.getElementById('result').innerText = 'Acertou Mizeravi...  N⍛' + numeroEscolhido;
+    button.style.backgroundColor = '#00cc00'; // Tornar o botão verde
+    document.getElementById('numberButtons').style.display = 'none';
+    document.getElementById('choppImg').classList.remove('hidden');
+
+    // Desabilitar os demais botões
+    for (let i = 0; i < numberButtons.length; i++) {
+      const button = numberButtons[i];
+      button.disabled = true;
+    }
   }
 }
 
